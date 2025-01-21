c43ae9950b7108fa69ebed2be844425e3415b6d3
Magaly <fernandezgarciamagaly@gmail.com>
2025-01-21 10:27:23 +0100

    adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ArticleList from "./containers/ArticleList";
 
-function App() {
-	return (
-		<>
-			<h1> WP React</h1>
-			<ArticleList />
-		</>
-	);
+
+const App =()=>{
+	<Router>
+		<Routes>
+		<Route path="/" element={<ArticleList />} />
+		</Routes>
+	</Router>
 }
+
+// function App() {
+// 	return (
+// 		<>
+// 			<h1> WP React</h1>
+// 			<ArticleList />
+// 		</>
+// 	);
+// }
 
 export default App;
